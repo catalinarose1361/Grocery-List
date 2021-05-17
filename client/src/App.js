ae03c0457391031fa6228d17568bbdfac462bd8b
Catalina Williams <webdevcatalina@gmail.com>
2021-05-17 14:50:04 -0600

    application sucessfully adds data to a database and dispays data on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const [groceries, setGroceries] = useState([
@@ -11,6 +12,14 @@ function App() {
     }
   ])
 
+  const [grocery, setGrocery] = useState(
+    {
+      item: '',
+      amount: '',
+      category: '',
+    }
+  )
+
     //fetch the data from the route and return a json
     useEffect(() => {
       fetch('/groceries').then(res => {
@@ -20,9 +29,44 @@ function App() {
         //take json response and set state equal to the json response
       }).then(jsonRes => setGroceries(jsonRes))
     })
-    
+   
+    function handleChange(e) {
+      //deconstruct the event
+      const {name, value} = e.target;
+
+      setGrocery(prevInput => {
+        return(
+          {
+            ...prevInput,
+            [name]: value
+            
+          }
+        )
+      })
+    }
+
+  function addGrocery(e) {
+    e.preventDefault();
+
+    const newGrocery = {
+      item: grocery.item,
+      amount: grocery.amount,
+      category: grocery.category
+    }
+
+    axios.post('/newgrocery', newGrocery)
+    alert("Grocery Item Added");
+  }
+
   return (
     <div className="App">
+      <h1>ADD ITEM TO GROCERY LIST</h1>
+      <form>
+        <input onChange={handleChange} name="item" value={grocery.item}></input>
+        <input onChange={handleChange} name="amount" value={grocery.amount}></input>
+        <input onChange={handleChange} name="category" value={grocery.category}></input>
+        <button onClick={addGrocery}>ADD ITEM</button>
+      </form>
       {groceries.map(grocery => {
         return (
         <div>
@@ -31,6 +75,7 @@ function App() {
           <p>{grocery.amount}</p>
           <p>{grocery.category}</p>
           <button>DELETE</button>
+
         </div>
         )
       })}
--- server.js
+++ server.js
@@ -25,10 +25,22 @@ const Grocery = mongoose.model("Grocery", grocerySchema);
 app.get('/groceries', function(req, res) {
     Grocery.find().then(groceries => res.json(groceries));
 })
-//test to see if server is running
-// app.get('/', function(req, res) {
-//     res.send("express is here")
-// })
+
+    //ADD GROCERY ITEM
+app.post('/newgrocery', function(req, res) {
+    const item = req.body.item;
+    const amount = req.body.amount;
+    const category = req.body.category;
+
+    const newGrocery = new Grocery({
+        item,
+        amount,
+        category
+    })
+
+    newGrocery.save();
+
+})
 
 app.listen(port, function() {
     console.log("EXPRESS SERVER is running");
