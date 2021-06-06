230a76402c2c0b3ca328ce975d9d6cad1cb7e0b2
Catalina Williams <webdevcatalina@gmail.com>
2021-06-06 14:54:57 -0600

    moved api routes to seperate file

---
+++ routes/api.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+
+//MONGOOSE MODEL IMPORT 
+const Grocery = require("../models")
+
+//FIND ALL MOVIES
+router.get('/groceries', function(req, res) {
+
+    //SEND A RESPONSE TO THE FRONT END IN JSON FORMAT
+    Grocery.find().then(grocery => res.json(groceries));
+});
+
+// ADD MOVIE
+
+//RECEIVING A POST REQUEST FROM THE FRONT END
+router.post('/newGrocery', function(req, res) {
+
+   //DECONSTRUCTING THE OBJECT SENT FROM THE FRONT END
+   const item = req.body.item;
+   const amount = req.body.amount;
+   const category = req.body.category;
+   
+   //CREATING A NEW MOVIE TO ADD TO MONGODB USING MOVIE MODEL
+   const newGrocery = new Grocery({
+       item,
+       amount,
+       category
+   })
+
+    //SAVING NEW MOVIE TO MONGODB ATLAS
+    newGrocery.save()
+})
+
+//REMOVE MOVIE
+router.delete('/delete/:id', function(req, res) {
+
+    //RECONSTRUCT ID SENT FROM FRONT END 
+    const id = req.params.id;
+
+    //WHEN THE ID OF THE MODEL MATCHES THIS ID, DELETE
+    Grocery.findByIdAndDelete({_id: id}, function(err) {
+        if(!err) {
+            console.log("Grocery Removed Sucessfully");
+        } else console.log("Error grocery cannot be removed")
+    })
+
+
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const routes = require("./routes/api.js")
 const port = process.env.PORT || 5000;
-const mongoose = require("mongoose");
-const Grocery = require("./models")
 
 require('dotenv').config()
 
@@ -16,46 +16,55 @@ app.use(cors());
 //MONGODB ATLAS CONNECTION STRING
 mongoose.connect(process.env.MONGODB_URI)
 
-//API routes
-app.get('/groceries', function(req, res) {
-    Grocery.find().then(groceries => res.json(groceries));
-})
+//API ROUTE CONNECTION
+app.use(routes);
 
-    //ADD GROCERY ITEM
-app.post('/newgrocery', function(req, res) {
-    const item = req.body.item;
-    const amount = req.body.amount;
-    const category = req.body.category;
-
-    const newGrocery = new Grocery({
-        item,
-        amount,
-        category
-    })
-
-    newGrocery.save();
-
-})
-
-    //DELETE ITEM
-    app.delete('/delete/:id', function(req, res) {
-         const id = req.params.id;
-         Grocery.findByIdAndDelete({_id: id}, function(err) {
-             if(!err) {
-                 console.log("item removed");
-             } else {
-                 console.log(err)
-            }
-        })
-    })
-
+//WHEN IN PRODUCTION, CONNECT THE BACK END WITH THE STATIC BUILD FILES
 if(process.env.NODE_ENV === 'production') {
+
     app.use(express.static('client/build'));
+
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
 app.listen(port, function() {
-    console.log("EXPRESS SERVER is running");
+
+    console.log("express is running");
 })
+
+//API routes
+// app.get('/groceries', function(req, res) {
+//     Grocery.find().then(groceries => res.json(groceries));
+// })
+
+//     //ADD GROCERY ITEM
+// app.post('/newgrocery', function(req, res) {
+//     const item = req.body.item;
+//     const amount = req.body.amount;
+//     const category = req.body.category;
+
+//     const newGrocery = new Grocery({
+//         item,
+//         amount,
+//         category
+//     })
+
+//     newGrocery.save();
+
+// })
+
+//     //DELETE ITEM
+//     app.delete('/delete/:id', function(req, res) {
+//          const id = req.params.id;
+//          Grocery.findByIdAndDelete({_id: id}, function(err) {
+//              if(!err) {
+//                  console.log("item removed");
+//              } else {
+//                  console.log(err)
+//             }
+//         })
+//     })
+
+
