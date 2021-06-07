10db18a39107edb9196bb0cec83fc3932511cf29
Catalina Williams <webdevcatalina@gmail.com>
2021-06-07 14:53:09 -0600

    styled Navigation Bar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Menu } from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
 
 function NavBar() {
     return (
-       <button>Go To Landing Page</button>
+        <Menu mode="horizontal">
+      
+    
+     
+        <Menu.Item icon={<HomeOutlined />}>
+          <a href="/" >
+            Home Page
+          </a>
+        </Menu.Item>
+      </Menu>
     )
 }
 
